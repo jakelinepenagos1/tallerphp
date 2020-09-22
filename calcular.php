8b076713d11dc4db5d0803ab2d18229d27a2a64f
jakelinepenagos1 <70981425+jakelinepenagos1@users.noreply.github.com>
2020-09-22 15:52:30 -0500

    diseño
    el nombre esta claro es todo lo del fondo y areglando algunos errores que puede tener antes

--- calcular.php
+++ calcular.php
@@ -10,8 +10,8 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.html"> 
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <a class="navbar-brand" href="index.php"> 
             <img src="img/candy.png" width="30" height="30"class="o-inline-block align-top" alt="logo1" loading="lazy">candy paradise
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,10 +21,14 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="index.php">pagina principal <span class="sr-only">(current)</span></a>
-                </li>
+                 
+                <a class="nav-link" href="calcular.php">calcular <span class="sr-only">(current)</span></a>
                 
-                  </a>
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php">pagina principal</a>
+                  </li>
+                
+                </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
@@ -32,6 +36,11 @@
                     <a class="dropdown-item" href="#">Something else here</a>
                   </div>
                 </li>
+                
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="busqueda" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
                 
               </form>
             </div>
--- index.php
+++ index.php
@@ -5,15 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> candy paradise  </title>
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="estilos.css">
+    
     
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.html"> 
-            <img src="img/candy.png" width="50" height="60"class="o-inline-block align-top" alt="logo1" loading="lazy">candy paradise
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <a class="navbar-brand" href="index.php"> 
+            <img src="img/candy.png" width="25" height="35"class="o-inline-block align-top" alt="logo1" loading="lazy">candy paradise 
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -22,13 +24,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">pagina principal <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="index.php">pagina principal <span class="sr-only">(current)</span></a>
                 
                 <li class="nav-item">
                   <a class="nav-link" href="calcular.php">calcular</a>
                 </li>
 
-                  </a>
+                </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
@@ -38,9 +40,14 @@
                 </li>
                 
               </ul>
-             
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="busqueda" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
+                
+              </form>
             </div>
           </nav>
+
     </header>
 
     <main>
@@ -89,7 +96,7 @@
                 <img class=imagen src="img/producto1.jpg" alt="producto">
                 <hr>
 
-                <h2>descripcion del producto</h2>
+                <h2 class="letra1">descripcion del producto</h2>
                 <h1>jweufowuuw fjfwjnfwnfew fwfenf</h1>
 
               </div>
