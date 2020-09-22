15cc3b4892a8ae2f101630ae8a1f0426efab0fff
jakelinepenagos1 <70981425+jakelinepenagos1@users.noreply.github.com>
2020-09-22 11:38:57 -0500

    trabajoparte1
    esto tiene todo lo funcional solo faltaria la decoracion como seria modificar algunas cosas para mejorar su apariencia a la vista

---
+++ calcular.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en"><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> candy paradise  </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html"> 
+            <img src="img/candy.png" width="30" height="30"class="o-inline-block align-top" alt="logo1" loading="lazy">candy paradise
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.php">pagina principal <span class="sr-only">(current)</span></a>
+                </li>
+                
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+                
+              </form>
+            </div>
+          </nav>
+    </header>
+
+    <main>
+    <div class="container">
+        
+        <div class="row justify-content-center mt-5">
+            <div class="col-4">
+
+                <form action="calcular.php" method="POST">
+                    <h3 class="text-center mb-4">Factura de Compra</h3>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Producto1" name="producto1">
+                        </div>
+                        <div class="col">
+                            <input type="number" class="form-control" placeholder="Precio($)" name="precio1">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Producto2" name="producto2">
+                        </div>
+                        <div class="col">
+                            <input type="number" class="form-control" placeholder="Precio($)" name="precio2">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Producto3" name="producto3">
+                        </div>
+                        <div class="col">
+                            <input type="number" class="form-control" placeholder="Precio($)" name="precio3">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Producto4" name="producto4">
+                        </div>
+                        <div class="col">
+                            <input type="number" class="form-control" placeholder="Precio($)" name="precio4">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Producto5" name="producto5">
+                        </div>
+                        <div class="col">
+                            <input type="number" class="form-control" placeholder="Precio($)" name="precio5">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block" name="botonCalcular">Calcular</button>
+                </form>
+
+              
+                
+                <?php if(isset($_POST["botonCalcular"])): ?>
+
+                
+                
+                    <h6 class="mt-5"> 
+                        <?php
+                            
+                            $precio1=$_POST["precio1"];
+                            $producto1=$_POST["producto1"];
+
+                            $precio2=$_POST["precio2"];
+                            $producto2=$_POST["producto2"];
+
+                            $precio3=$_POST["precio3"];
+                            $producto3=$_POST["producto3"];
+
+                            $precio4=$_POST["precio4"];
+                            $producto4=$_POST["producto4"];
+
+                            $precio5=$_POST["precio5"];
+                            $producto5=$_POST["producto5"];
+
+                            $total=$precio1+$precio2+$precio3+$precio4+$precio5;
+
+                            echo("El producto 1 es: ".$producto1." -----($) ".$precio1);
+                            echo("<br>");
+                            echo("El producto 2 es: ".$producto2." -----($) ".$precio2);
+                            echo("<br>");
+                            echo("El producto 3 es: ".$producto3." -----($) ".$precio3);
+                            echo("<br>");
+                            echo("El producto 4 es: ".$producto4." -----($) ".$precio4);
+                            echo("<br>");
+                            echo("El producto 5 es: ".$producto5." -----($) ".$precio5);
+                            echo("<br>");
+                            echo("<br>");
+                            echo("******************************");
+                            echo("<br>");
+                            echo("El total de la compra fué de: ".$total);
+                            
+
+
+                        ?>
+                    </h6>
+               
+                
+               
+                
+
+                <?php endif ?>
+
+                
+
+            </div>
+        </div>
+
+
+  
+    </main>
+
+    <footer>
+
+      <h3>derechos resevardos a </h3>
+      <h4>jakeline penagos escobar </h4>
+  
+     </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en"><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> candy paradise  </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
+    
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html"> 
+            <img src="img/candy.png" width="50" height="60"class="o-inline-block align-top" alt="logo1" loading="lazy">candy paradise
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">pagina principal <span class="sr-only">(current)</span></a>
+                
+                <li class="nav-item">
+                  <a class="nav-link" href="calcular.php">calcular</a>
+                </li>
+
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+                
+              </ul>
+             
+            </div>
+          </nav>
+    </header>
+
+    <main>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="img/imagen3.jpg!d" class="d-block w-100 carusel" alt="slider1">
+              </div>
+              <div class="carousel-item">
+                <img src="img/imagen5.jpg" class="d-block w-100 carusel" alt="slider2">
+              </div>
+              <div class="carousel-item">
+                <img src="img/imagen2.jpg!d" class="d-block w-100 carusel" alt="slider3">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+
+          <h1>bienvenidos al mundo de los postres </h1>
+
+            <hr>
+
+                 
+          <div class="container">
+
+            <div class="row">
+
+                <div class="col-4">
+
+                <h3>postre1</h3>
+
+                <hr>
+                <img class=imagen src="img/producto1.jpg" alt="producto">
+                <hr>
+
+                <h2>descripcion del producto</h2>
+                <h1>jweufowuuw fjfwjnfwnfew fwfenf</h1>
+
+              </div>
+
+              <div class="col-4">
+
+                <h3>postre2</h3>
+                <hr>
+                <img class=imagen src="img/producto2.jpg" alt="producto">
+                <hr>
+                <h2>descripcion del producto</h2>
+                 <h1>jweufow uuwfjfwjnfw nfewfwfenf</h1>
+
+              </div>
+              
+              <div class="col-4">
+
+                 <h3>postre3</h3>
+                 <hr>
+                <img class=imagen src="img/producto3.jpg" alt="producto">
+                <hr>
+
+                <h2>descripcion del producto</h2>
+                <h1>jweufowu uwfjfwjnfwnfe wfwfenf</h1>
+    
+                   </div>
+                   </div>
+                   </div>
+
+                        <hr>   
+          <div class="container">
+
+           <div class="row">
+            <div class="col-4">
+
+             <h3>postre4</h3>
+             <hr>
+             <img class=imagen src="img/producto4.jpg" alt="producto">
+             <hr>
+
+                <h2>descripcion del producto</h2>
+                <h1>jweufowuuw fjfwjnfwnfe wfwfenf</h1>
+    
+
+           </div>
+           <div class="col-4">
+
+          <h3>postre5</h3>
+          <hr>
+          <img class=imagen src="img/producto5.jpg" alt="producto">
+          <hr>
+
+                <h2>descripcion del producto</h2>
+                <h1>jweufowuu fjfwjnfwnfewf wfenf</h1>
+    
+
+            </div>
+                 </div>
+                     </div>
+    </main>
+
+    <footer>
+
+    <hr>
+
+      <h3>derechos resevardos a </h3>
+      <h4>jakeline penagos escobar </h4>
+  
+     </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+</body>
+</html>
